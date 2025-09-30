4f1d87d455853c7524331c27e65646f05baf77b5
Jana Kassem <janak@wireless-10-154-169-143.public.utexas.edu>
2025-09-30 10:17:54 -0500

    finish

--- app.R
+++ app.R
@@ -1,31 +1,49 @@
 library(shiny)
 
 ui <- fluidPage(
-    titlePanel("Let's Do Arithmetic!"),
-    sidebarLayout(
-        sidebarPanel(
-            numericInput(inputId = "number1",
-                         label = "Number 1",
-                         value = 0),
-            numericInput(inputId = "number2",
-                         label = "Number 2",
-                         value = 0)
-        ),
-        mainPanel(
-            h2("The sum of the two numbers is:"),
-            textOutput("sum")
-        )
+  titlePanel("Let's Do Arithmetic!"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("number1", "Number 1", value = 0),
+      numericInput("number2", "Number 2", value = 0),
+      actionButton("add", "Add"),           # NEW add button
+      actionButton("multiply", "Multiply")  # Multiply button
+    ),
+    mainPanel(
+      h2("The sum of the two numbers is:"),
+      textOutput("sum"),
+      h2("The product of the two numbers is:"),
+      textOutput("result")
     )
+  )
 )
 
 server <- function(input, output) {
-    output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
-    })
+  sum_val <- reactiveVal(0)       # store sum
+  product_val <- reactiveVal(0)   # store product
+  
+  # Add button
+  observeEvent(input$add, {
+    x1 <- as.numeric(input$number1)
+    x2 <- as.numeric(input$number2)
+    sum_val(x1 + x2)
+  })
+  
+  # Multiply button
+  observeEvent(input$multiply, {
+    x1 <- as.numeric(input$number1)
+    x2 <- as.numeric(input$number2)
+    product_val(x1 * x2)
+  })
+  
+  output$sum <- renderText({
+    sum_val()
+  })
+  
+  output$result <- renderText({
+    product_val()
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
 
